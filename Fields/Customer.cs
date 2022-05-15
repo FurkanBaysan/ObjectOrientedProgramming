93a2743541727ea8716640b10c18f4dce54c764c
FurkanBaysan <furkanbaysan1600166@gmail.com>
2022-05-15 16:48:07 +0300

    Fields(Attributes) of an Object

---
+++ Fields/Customer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fields
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+        public readonly List<Order> Orders = new List<Order>();
+
+        public Customer(int id)
+        {
+            this.Id = id;
+        }
+
+        public Customer(int id, string name) : this(id)
+        {
+            this.Name = name;
+        }
+
+        public void Promote()
+        {
+            //..
+        }
+    }
+}
---
+++ Fields/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Fields
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer(1);
+            customer.Orders.Add(new Order());
+            customer.Orders.Add(new Order());
+            customer.Promote();
+            Console.WriteLine("Number of orders: " + customer.Orders.Count);
+        }
+    }
+}
