97224f1af142554f1145dded5bdb447193879e28
FurkanBaysan <furkanbaysan1600166@gmail.com>
2022-05-16 20:36:32 +0300

    Access Modifiers and Encapsulation

---
+++ AccessModifiers/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers
+{
+    public class Person
+    {
+        private DateTime BirthDate;
+
+        public DateTime GetBirthDate()
+        {
+            return this.BirthDate;
+        }
+
+        public void SetBirthDate(DateTime birthDate)
+        {
+            if (!String.IsNullOrEmpty(birthDate.ToString()))
+            {
+                this.BirthDate = birthDate;
+            }
+        }
+    }
+}
---
+++ AccessModifiers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AccessModifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            DateTime dateTime =
+                new DateTime(1997, 7, 23);
+            person.SetBirthDate(dateTime);
+            Console.WriteLine("My Birth Date: " +
+                              person.GetBirthDate());
+        }
+    }
+}
