94571a9f10bf2852d0aedb4d01bb6d1ea04a8dc0
FurkanBaysan <furkanbaysan1600166@gmail.com>
2022-05-19 00:34:09 +0300

    Normal and Auto-Implemented Properties. Making properties read-only.

---
+++ Properties/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    public class Person
+    {
+        private string name;
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        public DateTime Birthdate
+        {
+            get; private set;
+        }
+
+        public Person(DateTime birthdate)
+        {
+            this.Birthdate = birthdate;
+        }
+        public int Age
+        {
+            get
+            {
+                TimeSpan timeSpan = DateTime.Today - this.Birthdate;
+                int years = timeSpan.Days / 365;
+
+                return years;
+            }
+        }
+
+    }
+}
---
+++ Properties/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person(new DateTime(1997, 7, 23, 10, 30, 0));
+            person.Name = "Furkan";
+            Console.WriteLine("Name: " + person.Name);
+            Console.WriteLine("Age: " + person.Age);
+        }
+    }
+}
