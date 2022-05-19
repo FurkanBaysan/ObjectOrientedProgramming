4d7e813b82f53d74976f1cbe45c00aded41ec7f2
FurkanBaysan <furkanbaysan1600166@gmail.com>
2022-05-19 05:01:48 +0300

    Indexer Concept Usage in a HttpCookie Scenario

---
+++ Indexers/HttpCookie.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexers
+{
+    public class HttpCookie
+    {
+        private readonly Dictionary<String, String> dictionary;
+        public DateTime Expiry { get; set; }
+
+        public HttpCookie()
+        {
+            dictionary = new Dictionary<String, String>();
+        }
+
+        public string this[string key]
+        {
+            get
+            {
+                return this.dictionary[key];
+            }
+            set
+            {
+                this.dictionary[key] = value;
+            }
+        }
+
+    }
+}
---
+++ Indexers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace Indexers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpCookie cookie = new HttpCookie();
+            cookie["name"] = "Furkan";
+            Console.WriteLine(cookie["name"]);
+        }
+    }
+}
