7f3007613731d5db69a1d56ed74a310b0fd36400
FurkanBaysan <furkanbaysan1600166@gmail.com>
2022-05-20 20:29:11 +0300

    Stopwatch Project Implementation

---
+++ StopwatchApplication/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace StopwatchApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            string input;
+            Console.WriteLine("Enter S to Start the Stopwatch" +
+                              " and Q to Stop");
+            input = Console.ReadLine();
+
+            while (!string.IsNullOrEmpty(input))
+            {
+                if (input.ToLower().Equals("S"))
+                {
+                    stopwatch.Start();
+                    Console.WriteLine("Enter Q the Stop the StopWatch");
+                }
+                else if (input.ToLower().Equals("Q"))
+                {
+                    Console.WriteLine(stopwatch.Stop());
+                    Console.WriteLine("Enter S to Start the StopWatch");
+                }
+                //After performing start and stop operations once, the user should be able to start and stop the stopwatch again any number of times without restarting the program.
+                //For this reason, we read input from the user before exiting the while block
+                //and repeat the above process.
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ StopwatchApplication/Stopwatch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopwatchApplication
+{
+    public class Stopwatch
+    {
+        public bool IsStart { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime StopTime { get; set; }
+        public TimeSpan Duration { get; set; }
+
+        public void Start()
+        {
+            if (IsStart)
+            {
+                throw new InvalidOperationException();
+            }
+            StartTime = DateTime.Now;
+            IsStart = true;//Indicating that stopwatch is now started.
+        }
+
+        public string Stop()
+        {
+            if (!IsStart)
+            {
+                throw new InvalidOperationException();
+            }
+            this.StopTime = DateTime.Now;
+            this.Duration = this.StopTime - this.StartTime;
+            IsStart = false;//indicating that stopwatch stopped
+
+            return Duration.Seconds.ToString();
+        }
+    }
+}
