b4c59f0e3034a88f9ec29c458019c183c3e840f5
FurkanBaysan <furkanbaysan1600166@gmail.com>
2022-05-20 21:42:42 +0300

    StackOverFlow Post Project Implementation

---
+++ StackOverFlowPostApplication/Post.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverFlowPostApplication
+{
+    public class Post
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime PostDate { get; set; }
+        public int Vote { get; private set; }
+
+        public Post(int vote)
+        {
+            this.Vote = vote;
+        }
+
+        public void UpVote()
+        {
+            this.Vote = this.Vote + 1;
+        }
+
+        public void DownVote()
+        {
+            if (this.Vote > 0)
+            {
+                this.Vote = this.Vote - 1;
+            }
+            else
+                throw new InvalidOperationException();
+        }
+
+        public int FetchCurrentVoteNumberOfPost()
+        {
+            return this.Vote;
+        }
+
+
+
+    }
+}
---
+++ StackOverFlowPostApplication/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StackOverFlowPostApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Post post = new Post(0);
+            post.UpVote();
+            post.UpVote();
+            Console.WriteLine("Post Vote: " + post.FetchCurrentVoteNumberOfPost());
+            post.DownVote();
+            post.DownVote();
+            Console.WriteLine("Post Vote: " + post.FetchCurrentVoteNumberOfPost());
+        }
+    }
+}
