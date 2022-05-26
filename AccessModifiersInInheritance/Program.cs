4464d205f68a53eeceac995b45bf05cce45ac0eb
FurkanBaysan <furkanbaysan1600166@gmail.com>
2022-05-26 15:24:26 +0300

    Use of Access Modifiers in Inheritance and an additional class library Amazon for explaining internal modifier

---
+++ AccessModifiersInInheritance/Program.cs
@@ -0,0 +1,13 @@
+﻿using Amazon;
+
+namespace AccessModifiersInInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            //Amazon.RateCalculator calculator = new RateCalculator();
+        }
+    }
+}
---
+++ Amazon/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Amazon
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public void Promote()
+        {
+            RateCalculator calculator = new RateCalculator();
+            int rating = calculator.Calculate(this);
+
+            //int rating = CalculateRating(true);
+            //if (rating == 0)
+            //{
+            //    Console.WriteLine("Promoted to Level 1");
+            //}
+            //else
+            //    Console.WriteLine("Promoted to Level 2");
+        }
+
+        private int CalculateRating(bool excludeOrders)
+        {
+            return 0;
+        }
+    }
+}
