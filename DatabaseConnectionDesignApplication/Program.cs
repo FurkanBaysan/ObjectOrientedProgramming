226745b54fe2188600dc6264ccc95b04458adcb0
FurkanBaysan <furkanbaysan1600166@gmail.com>
2022-05-31 16:23:05 +0300

    Polymorphism: Design a Database Connection Project Implementation

---
+++ DatabaseConnectionDesignApplication/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DatabaseConnectionDesignApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DbConnection oracleDbConnection = new OracleConnection("Oracle Connection String");
+            oracleDbConnection.Open();
+            oracleDbConnection.Close();
+
+            DbConnection sqlServerDbConnection = new SqlConnection("SqlServer Connection String");
+            sqlServerDbConnection.Open();
+            sqlServerDbConnection.Close();
+        }
+    }
+}
