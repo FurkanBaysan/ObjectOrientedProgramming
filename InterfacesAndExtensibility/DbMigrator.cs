1603c140d11bda520635de4bec4c21975aff57f5
FurkanBaysan <furkanbaysan1600166@gmail.com>
2022-06-05 17:45:10 +0300

    Interfaces and Extensibility: Open-Closed Principle (OCP)

---
+++ InterfacesAndExtensibility/DbMigrator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace InterfacesAndExtensibility
+{
+    public class DbMigrator
+    {
+        private readonly ILogger _logger;
+
+        public DbMigrator(ILogger logger)
+        {
+            _logger = logger;
+        }
+        public void Migrate()
+        {
+            _logger.LogInfo("Migrating started at {0} " + DateTime.Now);
+
+            //  Details of migrating the database.
+
+            _logger.LogInfo("Migrating finished at {0} " +
+                            DateTime.Today.AddDays(1));
+        }
+
+    }
+}
---
+++ InterfacesAndExtensibility/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace InterfacesAndExtensibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DbMigrator dbMigrator = new DbMigrator(new ConsoleLogger());
+            dbMigrator.Migrate();
+        }
+    }
+}
